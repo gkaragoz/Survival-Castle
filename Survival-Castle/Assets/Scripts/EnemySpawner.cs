9bcc8b12842625a6e1f9dbc66b52b7983f5c6783
gkaragoz <annotto3@gmail.com>
2019-04-30 21:48:00 +0300

    Create EnemySpawner script.

---
+++ Survival-Castle/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+
+    [SerializeField]
+    private float _spawnRate = 2f;
+    [SerializeField]
+    private float radius = 10f;
+    [SerializeField]
+    private GameObject _archerPrefab;
+
+    private Transform _target;
+
+    private void Awake() {
+        _target = GameObject.FindGameObjectWithTag("Target").transform;
+    }
+
+    private void Start() {
+        InvokeRepeating("Spawn", 1, _spawnRate);
+    }
+
+    private void Spawn() {
+        GameObject newArcher = Instantiate(_archerPrefab, this.transform);
+        newArcher.transform.position = RandomPointOnCircleEdge(radius);
+    }
+
+    private Vector3 RandomPointOnCircleEdge(float radius) {
+        Vector2 myPosition = new Vector2(transform.position.x, transform.position.z);
+        Vector2 pointOnEdge = myPosition + (Random.insideUnitCircle.normalized * radius);
+        return new Vector3(pointOnEdge.x, 0, pointOnEdge.y);
+    }
+
+    private void OnDrawGizmos() {
+        if (_target == null) {
+            _target = GameObject.FindGameObjectWithTag("Target").transform;
+        }
+
+        Gizmos.color = Color.red;
+
+        Gizmos.DrawWireSphere(_target.transform.position, radius);
+    }
+
+}
