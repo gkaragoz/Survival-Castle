f410546025148421307026838a55eee0e1041481
gkaragoz <annotto3@gmail.com>
2019-04-30 13:50:16 +0300

    Redesign the level.

---
+++ Survival-Castle/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyController : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject _target;
+
+    private NavMeshAgent _agent;
+    private Animator _animator;
+
+    private void Awake() {
+        _agent = GetComponent<NavMeshAgent>();
+        _animator = GetComponent<Animator>();
+    }
+
+    private void Start() {
+        _agent.SetDestination(_target.transform.position);
+    }
+
+    private void Update() {
+        _animator.SetFloat("Velocity", _agent.velocity.magnitude);
+    }
+
+}
