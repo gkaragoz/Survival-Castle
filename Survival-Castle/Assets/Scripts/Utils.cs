11e8da9793fbec9dcc5abe0b94ccf9eb4a376fd0
gkaragoz <annotto3@gmail.com>
2019-05-01 14:05:32 +0300

    Create Utils.cs script.

---
+++ Survival-Castle/Assets/Scripts/Utils.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Utils : MonoBehaviour {
+
+    [AttributeUsage(AttributeTargets.Field, Inherited = true)]
+    public class ReadOnlyAttribute : PropertyAttribute { }
+#if UNITY_EDITOR
+    [UnityEditor.CustomPropertyDrawer(typeof(ReadOnlyAttribute))]
+    public class ReadOnlyAttributeDrawer : UnityEditor.PropertyDrawer {
+        public override void OnGUI(Rect rect, UnityEditor.SerializedProperty prop, GUIContent label) {
+            bool wasEnabled = GUI.enabled;
+            GUI.enabled = false;
+            UnityEditor.EditorGUI.PropertyField(rect, prop);
+            GUI.enabled = wasEnabled;
+        }
+    }
+#endif
+
+    public static bool IsPointerOverUIObject() {
+        PointerEventData eventDataCurrentPosition = new PointerEventData(EventSystem.current);
+        eventDataCurrentPosition.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        List<RaycastResult> results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(eventDataCurrentPosition, results);
+        return results.Count > 0;
+    }
+
+}
