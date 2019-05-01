8bc7075c3b33013272652f8b8a69ed3f166f9399
gkaragoz <annotto3@gmail.com>
2019-05-01 17:39:19 +0300

    Create LogManager and Log.cs scripts.

---
+++ Survival-Castle/Assets/Scripts/Logger/Log.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+
+[System.Serializable]
+public class Log {
+
+    private LogType _logType;
+    private string _body;
+    private string _message;
+    private DateTime _dateTime;
+
+    public Log(string message, DateTime dateTime, LogType logType) {
+        this._message = message;
+        this._dateTime = dateTime;
+        this._logType = logType;
+
+        string _title = string.Empty;
+
+        this._body = string.Format("[{0}] {1} {2}",
+                            this._dateTime.ToLongTimeString(), 
+                            _title,
+                            this._message);
+    }
+
+    public string GetBody() {
+        return this._body;
+    }
+
+    public string GetMessage() {
+        return this._message;
+    }
+
+    public string GetFormattedDateTime() {
+        return string.Format("{0:d/M/yyyy HH:mm:ss}", this._dateTime);
+    }
+
+}
---
+++ Survival-Castle/Assets/Scripts/Logger/LogManager.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogManager : MonoBehaviour {
+
+    #region Singleton
+
+    public static LogManager instance;
+
+    void Awake() {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+    }
+
+    #endregion
+
+    private Queue<Log> _logs = new Queue<Log>();
+
+    private void Start() {
+        Application.logMessageReceived += LogCallback;
+    }
+
+    private void OnDestroy() {
+        Application.logMessageReceived -= LogCallback;
+    }
+
+    public void AddLog(string message, LogType type) {
+        Log log = new Log(
+            message,
+            DateTime.Now,
+            type);
+
+        _logs.Enqueue(log);
+    }
+
+    private void LogCallback(string condition, string stackTrace, LogType type) {
+        if (type == LogType.Log) {
+            AddLog(condition, LogType.Log);
+        } else {
+            AddLog("LogType: " + type, type);
+            AddLog("Condition: " + condition, type);
+            AddLog("StackTrace: " + stackTrace, type);
+        }
+    }
+
+}
