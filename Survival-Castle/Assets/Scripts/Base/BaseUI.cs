097247cc607aead766a1229edb7e22d34768a816
gkaragoz <annotto3@gmail.com>
2019-05-03 14:36:16 +0300

    Create and Implement BaseUI.cs script.

---
+++ Survival-Castle/Assets/Scripts/Base/BaseUI.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(BaseController), typeof(Base))]
+public class BaseUI : Menu {
+
+    [SerializeField]
+    private Slider _slider;
+
+    private BaseController _baseController;
+    private Base _baseStats;
+
+    private void Awake() {
+        _baseController = GetComponent<BaseController>();
+        _baseStats = GetComponent<Base>();
+
+        _baseController.onTakeDamage += OnTakeDamage;
+        _baseController.onDead += OnDead;
+    }
+
+    private void OnDestroy() {
+        _baseController.onTakeDamage -= OnTakeDamage;
+        _baseController.onDead -= OnDead;
+    }
+
+    private void Start() {
+        _slider.maxValue = _baseStats.GetMaxHealth();
+        _slider.value = _baseStats.GetCurrentHealth();
+    }
+
+    private void OnTakeDamage() {
+        _slider.value = _baseStats.GetCurrentHealth();
+    }
+
+    private void OnDead() {
+        LogManager.instance.AddLog("[BASE] has been destroyed!");
+    }
+
+}
